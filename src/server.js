81735ed78da3474601ecec5cb52f921f1c5d2720
Parth <112978352+parthhhhh12@users.noreply.github.com>
2025-10-28 12:58:01 +0530

    Set up basic Express server on EC2

--- src/server.js
+++ src/server.js
@@ -1,1 +1,6 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Hello from Node.js on EC2!'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
